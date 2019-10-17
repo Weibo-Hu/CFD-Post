f698af7ba1190ade5c1caee373b4ca59707f666e
Weibo <weibo-hu@outlook.com>
2019-10-17 15:52:22 +0200

    Fix a bug of missing some blocks when calculating the mean flow

--- script/tp_stat2hdf.py
+++ script/tp_stat2hdf.py
@@ -17,11 +17,13 @@ import pandas as pd
 from glob import glob
 
 # %%
-path = '/media/weibo/VID2/BFS_M1.7TS/'
+path = '/media/weibo/VID2/BFS_M1.7L/'
+
+p2p.create_folder(path)
 pathTP = path + 'TP_stat/'
 pathM = path + 'MeanFlow/'
 pathT = path + 'TimeAve/'
-dirs = glob(pathTP + '*.plt')
+dirs = glob(pathTP + '*plt')
 equ = ['{|gradp|}=sqrt(ddx({<p>})**2+ddy({<p>})**2+ddz({<p>})**2)']
 
 num = np.size(dirs)
@@ -29,9 +31,9 @@ a1 = int(num / 4)
 a2 = a1 * 2
 a3 = a1 * 3
 ind = [[0, a1],
-       [a1 + 1, a2],
-       [a2 + 1, a3],
-       [a3 + 1, num - 1]]
+       [a1, a2],
+       [a2, a3],
+       [a3, num]]
 for i in range(4):
     FileList = dirs[ind[i][0]:ind[i][1]]
     df = p2p.ReadAllINCAResults(pathTP,
@@ -39,11 +41,11 @@ for i in range(4):
                                 FileName=FileList,
                                 Equ=equ,
                                 OutFile='MeanFlow' + str(i))
-
-df0 = pd.read_hdf(pathT + 'MeanFlow0.h5')
-df1 = pd.read_hdf(pathT + 'MeanFlow1.h5')
-df2 = pd.read_hdf(pathT + 'MeanFlow2.h5')
-df3 = pd.read_hdf(pathT + 'MeanFlow3.h5')
+dir1 = glob(pathT + 'MeanFlow*')
+df0 = pd.read_hdf(dir1[0])
+df1 = pd.read_hdf(dir1[1])
+df2 = pd.read_hdf(dir1[2])
+df3 = pd.read_hdf(dir1[3])
 df = pd.concat([df0, df1, df2, df3], ignore_index=True)
 # df = df.drop_duplicates(keep='last')
 # save time- and spanwise averaged flow field
@@ -51,5 +53,5 @@ grouped = df.groupby(['x', 'y'])
 mean = grouped.mean().reset_index()
 mean.to_hdf(pathM + 'MeanFlow.h5', "w", format="fixed")
 # save time-averaged flow field
-new = df.loc[(df['x']>=-5.0) & (df['x']<=20.0) & (df['y']<=2.0)]
-new.to_hdf(pathT + 'TimeAve.h5', "w", format="fixed")
+# new = df.loc[(df['x']>=-40.0) & (df['x']<=20.0) & (df['y']<=2.0)]
+# new.to_hdf(pathT + 'TimeAve.h5', "w", format="fixed")
