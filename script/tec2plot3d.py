3c82e458dfb1c35abcf4400aa361db195e115fbc
Weibo <Weibo-Hu@outlook.com>
2022-11-29 14:59:34 +0800

    fix bugs in tec2plot3d

--- script/tec2plot3d.py
+++ script/tec2plot3d.py
@@ -14,12 +14,15 @@ from scipy.interpolate import griddata
 from scipy.io import FortranFile
 
 # %%
-def csv2plot3d(path, filenm, vars, option='2d'):
-    data = pd.read_csv(path + filenm, sep='\t')
-    vars = ['x', 'y', 'u', 'v', 'w', 'rho', 'p', 'T']
+def csv2plot3d(path, filenm, vars, option='2d', input_nm=None, skip=1):
+    if input_nm is None:
+        data = pd.read_csv(path + filenm, sep=',', index_col=False)
+    else:
+        data = pd.read_csv(path + filenm, sep=',', index_col=False,
+                           header=0, names=input_nm)
     grid = 'grid.grd'
     solu = 'uvw.q'
-    xa = np.unique(data['x'])
+    xa = np.unique(data['x'][::skip])
     ya = np.unique(data['y'])
     xmat, ymat = np.meshgrid(xa, ya)
     # organize grid
@@ -31,25 +34,26 @@ def csv2plot3d(path, filenm, vars, option='2d'):
     if option=='3d':
         zmat = griddata((data['x'], data['y']), data['z'], (xmat, ymat))
         df_grid = np.vstack((df_grid, zmat.flatten(order='C')))
-        np.append(dim, 1)
+        dim = np.append(dim, 1)
     # output grid
     file = FortranFile(path + grid, 'w')
     file.write_record(np.array([1], dtype=np.int32))
-    file.write_record(dim)
+    file.write_record(np.array([dim], dtype=np.int32))
     file.write_record(df_grid)
     file.close()
     # organize solution
     for j in range(np.size(vars)):
-        umat = griddata((data['x'], data['y']), data[vars[j]], (xmat, ymat))
+        umat = griddata((data['x'], data['y']), data[vars[j]],
+                        (xmat, ymat))
         if j == 0:
-            df_data = umat.flatten(order='F')
+            df_data = umat.flatten(order='C')
         else:
-            df_data = np.vstack((df_data, umat.flatten(order='F')))
+            df_data = np.vstack((df_data, umat.flatten(order='C')))
     nvar = np.size(vars)
-    np.append(dim, nvar)
+    dim_var = np.append(dim, nvar)
     file = FortranFile(path + solu, 'w')
     file.write_record(np.array([1], dtype=np.int32))
-    file.write_record(dim)
+    file.write_record(np.array([dim_var], dtype=np.int32))
     file.write_record(df_data)
     file.close()
 
@@ -60,10 +64,11 @@ def interp2d(x, y, z):
     ya = np.unique(y)
     xmat, ymat = np.meshgrid(xa, ya)
     zmat = griddata((x, y), z, (xmat, ymat))
-    return(zmat)
+    return(zmat)     
 
 
-path = '/mnt/share/cases/base/'
-filenm = 'slice.txt'
-vars = ['x', 'y', 'u', 'v', 'w', 'rho', 'p', 'T']
-csv2plot3d(path, filenm, vars, option='2d')
+path = 'E:/cases/'
+filenm = 'slice_3d.dat'
+nms = ['x', 'y', 'z', 'u', 'v', 'w', 'rho', 'p', 'T']
+vars = ['rho', 'u', 'v', 'w', 'T', 'p']
+csv2plot3d(path, filenm, vars, option='3d', input_nm=nms, skip=2)
