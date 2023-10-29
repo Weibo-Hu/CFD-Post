9376c6de05ea106d304280c06ed01152fa4c26a7
Weibo <weibo-hu@outlook.com>
2023-10-29 17:48:14 +0800

    produce base flow from mean flow field

--- script/tec2plot3d.py
+++ script/tec2plot3d.py
@@ -13,6 +13,7 @@ import numpy as np
 from scipy.interpolate import griddata
 from scipy.io import FortranFile
 import os
+import sys
 
 # %%
 def csv2plot3d(path, filenm, vars, option='2d', input_nm=None, skip=1):
@@ -31,7 +32,7 @@ def csv2plot3d(path, filenm, vars, option='2d', input_nm=None, skip=1):
                                header=0, names=input_nm)            
     grid = 'grid.xyz'
     solu = 'uvw3D.q'
-    xa = np.unique(data['x'][::skip])
+    xa = np.unique(data['x'])[::skip]
     ya = np.unique(data['y'])
     xmat, ymat = np.meshgrid(xa, ya)
     # organize grid
@@ -75,13 +76,26 @@ def interp2d(x, y, z):
     zmat = griddata((x, y), z, (xmat, ymat))
     return(zmat)     
 
+def create_meanval(path, filenm):
+    if not os.path.isfile(path + filenm):
+        sys.exit('file is not exist!')
+    df = pd.read_hdf(path + filenm)
+    df['u']   = df['<u>']
+    df['v']   = df['<v>']
+    df['w']   = df['<w>']
+    df['rho'] = df['<rho>']
+    df['p']   = df['<p>']
+    df['T']   = df['<T>']
+    df.to_hdf(path + filenm, 'w', format='fixed')
+    return(df)
 
 # %%
 if __name__ == "__main__":
     # path = 'E:/cases/'
     # filenm = 'slice_3d.dat'
-    path = 'E:/cases/flat_SFD/'
-    filenm = 'Baseflow_02000.00.h5'
+    path = 'E:/cases/flat_base/'  
+    create_meanval(path + 'MeanFlow/', 'MeanFlow.h5')
+    filenm = 'MeanFlow/MeanFlow.h5'
     nms = ['x', 'y', 'z', 'u', 'v', 'w', 'rho', 'p', 'T']
     vars = ['rho', 'u', 'v', 'w', 'T', 'p']
-    csv2plot3d(path, filenm, vars, option='3d', input_nm=nms, skip=1)
+    csv2plot3d(path, filenm, vars, option='2d', input_nm=nms, skip=4)
