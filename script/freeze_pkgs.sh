075a49f5067eb577f5595001cd06015062e529c7
Weibo <weibo-hu@outlook.com>
2024-09-05 10:57:56 +0800

    freeze pkgs

--- script/freeze_pkgs.sh
+++ script/freeze_pkgs.sh
@@ -1,4 +1,9 @@
+## output the current pkgs with version number
 pip3 freeze > requirements.txt
+## download the pkgs based on requirements
 # pip download -d DIR -r requirements.txt
 pip wheel -w DIR -r requirements.txt
+## install the required pkgs offline
 pip3 install --no-index --find-links=DIR -r requirements.txt
+## install the required pkgs onlin
+pip3 install -r requirements.txt
