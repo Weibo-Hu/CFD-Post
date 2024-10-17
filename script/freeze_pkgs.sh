6467d96781df904ab61cd397b38c13442b819230
Weibo <weibo-hu@outlook.com>
2024-10-17 11:13:23 +0800

    freeze the required pkgs with specific verisons

--- script/freeze_pkgs.sh
+++ script/freeze_pkgs.sh
@@ -1,4 +1,5 @@
-pip3 freeze > requirements.txt
+pip3 freeze > requirements.txt   # save the current pkgs
 # pip download -d DIR -r requirements.txt
-pip wheel -w DIR -r requirements.txt
-pip3 install --no-index --find-links=DIR -r requirements.txt
+pip wheel -w DIR -r requirements.txt  # download all the pkgs
+pip3 install --no-index --find-links=DIR -r requirements.txt  # install all the pkgs offline
+pip3 install -r requirements.txt # install all the pkgs online
